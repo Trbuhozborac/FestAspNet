b74495e0206235756071c881bb77a3794a23cfad
Trbuhozborac <nemanjapipi@hotmail.com>
2020-02-20 14:15:38 +0100

    Initial commit

---
+++ Controllers/IzvodjaciController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrogMusicFest.Controllers
+{
+    public class IzvodjaciController : Controller
+    {
+        // GET: Izvodjaci
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FrogMusicFest.MvcApplication" Language="C#" %>
